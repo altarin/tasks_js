9d361266d6bd729fd6d7262084c1b9d5382b1797
Natalia <alt.antares@gmail.com>
2024-07-13 10:31:35 +0300

    solved 9 task

--- Objects/script.js
+++ Objects/script.js
@@ -2,14 +2,14 @@
 // Создайте класс Worker. Класс должен содержать свойства name, surname.
 // Создайте на его основе объект worker. Выведите в консоль созданный объект.
 
-class Worker {
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
-    }
-}
-const worker = new Worker('Имя', 'Фамилия');
-console.log(worker);
+// class Worker {
+//     constructor(name, surname) {
+//         this.name = name;
+//         this.surname = surname;
+//     }
+// }
+// const worker = new Worker('Имя', 'Фамилия');
+// console.log(worker);
 
 
 // ----------------------------------------------------------------------------------------------------------------
@@ -20,17 +20,17 @@ console.log(worker);
 // который будет выводить в консоль информацию о книге в
 // формате: "Название: название книги, Автор: автор книги, Год издания: год издания книги".
 
-const book = {
-    title: "Преступление и наказание",
-    author: "Федор Достоевский",
-    year: 1866
-};
+// const book = {
+//     title: "Преступление и наказание",
+//     author: "Федор Достоевский",
+//     year: 1866
+// };
 
-book.getBookInfo = function() {
-    console.log("Название: " + this.title + ", Автор: " + this.author + ", Год издания: " + this.year);
-};
+// book.getBookInfo = function() {
+//     console.log("Название: " + this.title + ", Автор: " + this.author + ", Год издания: " + this.year);
+// };
 
-book.getBookInfo();
+// book.getBookInfo();
 
 
 // -------------------------------------------------------------------------------------------------------------------
@@ -53,27 +53,27 @@ book.getBookInfo();
 //Выведите на экран инормацию о товарах в консоль
 
 
-//Конструктор товара
-function Product(name, price) {
-    this.name = name;
-    this.price = price;
-}
+// //Конструктор товара
+// function Product(name, price) {
+//     this.name = name;
+//     this.price = price;
+// }
 
-//Метод для вывода информации о товаре
-Product.prototype.displayInfo = function () {
-    console.log(`Название: ${this.name}, Цена: ${this.price}`);
-}
+// //Метод для вывода информации о товаре
+// Product.prototype.displayInfo = function () {
+//     console.log(`Название: ${this.name}, Цена: ${this.price}`);
+// }
 
 //Создаём несколько товаров
-const item1 = new Product("Футболка", 25);
-const item2 = new Product("Джинсы", 50);
-const item3 = new Product("Кроссовки", 80);
+// const item1 = new Product("Футболка", 25);
+// const item2 = new Product("Джинсы", 50);
+// const item3 = new Product("Кроссовки", 80);
 
-//Выводим информацию о товарах
-console.log("Информация о товарах:");
-item1.displayInfo();
-item2.displayInfo();
-item3.displayInfo();
+// //Выводим информацию о товарах
+// console.log("Информация о товарах:");
+// item1.displayInfo();
+// item2.displayInfo();
+// item3.displayInfo();
 
 // -----------------------------------------------------------------------------------------------------------
 
@@ -82,18 +82,18 @@ item3.displayInfo();
 // Добавьте в класс Worker метод fullName, который будет возвращать полное имя работника в формате "Фамилия, Имя".
 // Создайте объект worker на основе класса Worker и выведите его полное имя в консоль с помощью метода fullName
 
-class Worker {
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
-    }
+// class Worker {
+//     constructor(name, surname) {
+//         this.name = name;
+//         this.surname = surname;
+//     }
 
-    fullName () {
-        return this.surname + ', ' + this.name;
-    }
-}
-const worker2 =  new Worker('Имя', 'Фамилия');
-console.log(worker2.fullName());
+//     fullName () {
+//         return this.surname + ', ' + this.name;
+//     }
+// }
+// const worker2 =  new Worker('Имя', 'Фамилия');
+// console.log(worker2.fullName());
 
 
 // -----------------------------------------------------------------------------------------------------------------
@@ -103,15 +103,15 @@ console.log(worker2.fullName());
 // Создайте класс Car. Класс должен содержать свойства brand, model и year.
 // Создайте на его основе объект car и выведите его свойства в консоль.
 
-class Car {
-    constructor (brand, model, year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-    }
-}
-const car2 = new Car('Бренд', 'Модель', 'Год');
-console.log(car2);
+// class Car {
+//     constructor (brand, model, year) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.year = year;
+//     }
+// }
+// const car2 = new Car('Бренд', 'Модель', 'Год');
+// console.log(car2);
 
 
 // -----------------------------------------------------------------------------------------------------------------
@@ -123,21 +123,21 @@ console.log(car2);
 // Создайте объект car2 на основе класса Car и выведите его возраст в консоль с помощью метода getAge.
 // Результат в консоли должен быть типом данных numbers.
 
-class Car {
-    constructor(brand, model, year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.getAge();
-    }
-    getAge(){
-        const nowYears = new Date().getFullYear();
-        return nowYears - this.year;
-    }
-}
+// class Car {
+//     constructor(brand, model, year) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.year = year;
+//         this.getAge();
+//     }
+//     getAge(){
+//         const nowYears = new Date().getFullYear();
+//         return nowYears - this.year;
+//     }
+// }
 
-const car = new Car('BMW','X6',2023);
-console.log(car.getAge());
+// const car = new Car('BMW','X6',2023);
+// console.log(car.getAge());
 
 
 // -----------------------------------------------------------------------------------------------------------------
@@ -146,14 +146,14 @@ console.log(car.getAge());
 // Cоздайте класс Rectangle. Класс должен содержать свойства width и height.
 // Создайте на его основе объект rectangle и выведите его свойства в консоль.
 
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-}
-const rectangle = new Rectangle('ширина', 'высота')
-console.log(rectangle)
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+// }
+// const rectangle = new Rectangle('ширина', 'высота')
+// console.log(rectangle)
 
 // -----------------------------------------------------------------------------------------------------------------
 
@@ -162,18 +162,18 @@ console.log(rectangle)
 // (произведение ширины и высоты). Создайте объект rectangle на основе класса Rectangle и
 // выведите его площадь в консоль с помощью метода getArea.
 
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.getArea();
-    }
-    getArea(){
-        return this.width * this.height;
-    }
-}
-const rectangle = new Rectangle();
-console.log(rectangle.getArea());
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//         this.getArea();
+//     }
+//     getArea(){
+//         return this.width * this.height;
+//     }
+// }
+// const rectangle = new Rectangle();
+// console.log(rectangle.getArea());
 
 // -----------------------------------------------------------------------------------------------------------------
 
@@ -181,13 +181,13 @@ console.log(rectangle.getArea());
 // Cоздайте класс Circle. Класс должен содержать свойство radius.
 // Создайте на его основе объект circle и выведите его свойство в консоль.
 
-class Circle {
-    constructor(radius){
-        this.radius = radius;
-    }
-}
-const circle = new Circle();
-console.log(circle);
+// class Circle {
+//     constructor(radius){
+//         this.radius = radius;
+//     }
+// }
+// const circle = new Circle();
+// console.log(circle);
 
 // -----------------------------------------------------------------------------------------------------------------
 
@@ -196,17 +196,29 @@ console.log(circle);
 //     Формула для расчета площади круга: площадь = радиус * радиус * 3.14. Создайте объект circle2 на основе класса
 // Circle с радиусом 3 и выведите его площадь круга в консоль с помощью метода calculateArea.
 
-class Circle {
-    constructor(radius){
-        this.radius = radius
-    }
-    calculateArea(){
-        return this.radius * this.radius * 3.14;
-    }
-}
-const circle = new Circle(12)
-console.log(circle.calculateArea());
+// class Circle {
+//     constructor(radius){
+//         this.radius = radius
+//     }
+//     calculateArea(){
+//         return this.radius * this.radius * 3.14;
+//     }
+// }
+// const circle = new Circle(12)
+// console.log(circle.calculateArea());
 
 // -----------------------------------------------------------------------------------------------------------------
 
 // 9
+// Создайте класс Student. Класс должен содержать свойства name, age и grade. 
+// Создайте на его основе объект student и выведите его свойства в консоль.
+
+class Student {
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }     
+}
+const student = new Student('Маша', 20, 5);
+console.log(student);
