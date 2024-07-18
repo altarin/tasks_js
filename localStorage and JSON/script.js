61f09a6301fc65b8e26841fc14a4d9cec248dc46
Natalia <alt.antares@gmail.com>
2024-07-18 22:18:51 +0300

    solved 3 task

--- localStorage and JSON/script.js
+++ localStorage and JSON/script.js
@@ -1,15 +1,19 @@
 // 1
 // Создайте функцию makeOne которая записывает в Local Storage ключ games со значением 11 и вызовите ее.
 
-function makeOne() {
-    window.localStorage.setItem('games', '11');
-}
-makeOne()
+// function makeOne() {
+//     window.localStorage.setItem('games', '11');
+// }
+// makeOne()
 
 //   2
 // Создайте функцию makeTwo, которая сохраняет в Local Storage строку "7,6,5". Ключ назовите numbers. После чего вызовите функцию.
 
-function makeTwo() {
-    window.localStorage.setItem('numbers', '7,6,5');
-  }
-  makeTwo()
+// function makeTwo() {
+//     window.localStorage.setItem('numbers', '7,6,5');
+//   }
+//   makeTwo()
+
+//   3
+// Создайте функцию makeThree, которая сохраняет в Local Storage строку "name=John,age=25".
+//  Ключ назовите users, после чего вызовите функцию.
