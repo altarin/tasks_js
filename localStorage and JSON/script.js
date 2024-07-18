9795c3dcc22e29d939f95e59c6cbada387ea58f6
Natalia <alt.antares@gmail.com>
2024-07-18 22:14:23 +0300

    solved 1 task

---
+++ localStorage and JSON/script.js
@@ -0,0 +1,7 @@
+// 1
+// Создайте функцию makeOne которая записывает в Local Storage ключ games со значением 11 и вызовите ее.
+
+function makeOne() {
+    window.localStorage.setItem('games', '11')
+  }
+  makeOne()
